7242f451aa00c0f214705dceb0d8da283fb28104
Julian Mazzitelli <mazzitelli.julian@gmail.com>
2016-11-23 12:06:11 -0500

    replace acid:four in key to namespace prefix

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,61 @@
 'use strict'
 
-const path = require('path')
+import * as path from 'path'
 const fs = require('fs-promise')
 const yaml = require('js-yaml-promise')
 const debug = require('debug')('schema-salad')
 
-module.exports = (filename) =>
+const load = (filename: string) =>
   fs.readFile(filename, 'utf8')
-  .then((contents) => {
-    const ext = path.extname(filename)
+    .then((contents: string) => {
+      const ext = path.extname(filename)
 
-    if (ext.match(/\.ya?ml/)) {
-      return yaml.safeLoad(contents)
-    } else if (ext === '.json') {
-      return JSON.parse(contents)
-    } else {
-      throw new Error('Was not a yaml or json document')
-    }
-  })
-  .then((obj) => {
-    console.log('Received: ')
-    console.log(JSON.stringify(obj, null, 2))
-  })
-  .catch(console.error)
+      if (ext.match(/\.ya?ml/)) {
+        return yaml.safeLoad(contents)
+      } else if (ext === '.json') {
+        return JSON.parse(contents)
+      } else {
+        throw new Error('Was not a yaml or json document')
+      }
+    })
+    .catch(console.error)
+
+const process = (schema) => (filename: string) =>
+  load(filename)
+    .then((obj) => {
+      console.log('Supposed to process:')
+      console.log(obj)
+
+      const newObj = Object.keys(obj).reduce((acc, key) => {
+        let keyVal = obj[key]
+
+        if (/^\w+:\w+$/.test(key)) {
+          console.log('Caught: ', key)
+          console.log('Can we match it?')
+          console.log(schema.$namespaces)
+          console.log('Matches to:')
+          const nspace = key.replace(/:\w+/, '')
+          console.log(schema.$namespaces[nspace])
+
+          key = key.replace(/^\w+:/, schema.$namespaces[nspace])
+        }
+
+        return Object.assign({}, acc, { [key]: keyVal })
+      }, {})
+
+      console.log('Made:')
+      console.log(JSON.stringify(newObj, null, 2))
+
+      return newObj
+    })
+
+const loadSchema = (filename: string) =>
+  load(filename)
+    .then((obj) => {
+      console.log('Received: ')
+      console.log(JSON.stringify(obj, null, 2))
+
+      return process(obj)
+    })
+
+module.exports = loadSchema
--- test/test.spec.js
+++ test/test.spec.js
@@ -17,4 +17,22 @@ describe('SALAD', function () {
       .then(() => done())
       .catch(console.error)
   })
+
+  describe('Field name resolution', function () {
+    it.only('Should replace namespace prefix', function(done) {
+      salad(path.resolve(__dirname, './schema.yaml'))
+        // .then((process) => console.log(process))
+        .then((process) => process(path.resolve(__dirname, './field-name-resolution-example.json')))
+        .then((obj) => done(assert.deepEqual(obj, {
+          base: 'one',
+          form: {
+            "http://example.com/base": 'two',
+            "http://example.com/three": 'three'
+          },
+          'http://example.com/acid#four': 'four'
+        })))
+        // .then(() => done())
+        .catch(console.error)
+    })
+  })
 })
